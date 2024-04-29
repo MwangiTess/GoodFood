ce05e6b71e14796e2ae679ea96d0eaecf3f6e206
Teresia <teresiamwangi26@gmail.com>
2024-04-29 12:57:45 +0300

    added and modified sidebar css

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -16,43 +16,43 @@ export const Sidebar = ({}: Props) => {
             <ul className='sidebar-list'>
             <p className='title'>MENU</p>
                 <li className='sidebar-list-item'>
-                    <a href="" className='dashboard'>
+                    <a href="./" className='dashboard'>
                     <img src={dashboard} alt="icon" /> Dashboard
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                         <img src={foodorder} alt="icon" /> Food Order
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                         <img src={managemenu} alt="icon" /> Manage Menu
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                         <img src={customerreview} alt="icon" /> Customer Review
                     </a>
                 </li>
                 <p className='title'>OTHERS</p>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                         <img src={settings} alt="icon" />  Settings
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                          <img src={payments} alt="icon" /> Payments
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                          <img src={accounts} alt="icon" /> Accounts
                     </a>
                 </li>
                 <li className='sidebar-list-item'>
-                    <a href="">
+                    <a href="./">
                          <img src={help} alt="icon" /> Help
                     </a>
                 </li>
